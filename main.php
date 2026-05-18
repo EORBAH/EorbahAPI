be404561b6c995db76dd96aa577f13cd6f39e0f7
EORBAH <elle.oumarrafiou@gmail.com>
2026-05-18 13:28:05 +0000

    ajout de test et d'exemples

--- main.php
+++ main.php
@@ -5,6 +5,7 @@ require __DIR__ . '/vendor/autoload.php';
 use EorBah545\Eorbahapi\Request;
 use EorBah545\Eorbahapi\Response;
 use EorBah545\Eorbahapi\EorbahAPI;
+use EorBah545\Eorbahapi\StaticFiles;
 use EorBah545\Eorbahapi\rpc\JsonRPC;
 use EorBah545\Eorbahapi\ExceptionHandlers;
 use EorBah545\Eorbahapi\Exceptions\HTTPException;
@@ -15,9 +16,7 @@ $app = new EorbahAPI();
 $exceptionHandlers = new ExceptionHandlers();
 $exceptionHandlers->overrideExceptionHandlers($app);
 
-$app->get('/', function(Response $res) {
-    $res->send("Hello world");
-});
+
 
 $app->get('/api/{version}', function(Response $res, Request $req, $version) {
     $params = $req->params();
@@ -41,5 +40,7 @@ $rpc->add_method('getUser', function (Request $req, int $userId) {
 
 // Dans EorbahAPI, on monte le serveur sur une route
 $app->mount('/rpc', $rpc);
+
+$app->mount("/docs", new StaticFiles("docs/"));
 
 $app->run();
