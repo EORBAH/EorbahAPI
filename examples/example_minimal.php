8a2b7e2cbdfe2d23b171095bd27343f64c8819ed
EORBAH <182552823+EORBAH@users.noreply.github.com>
2026-08-13 22:16:18 +0000

    refactor: Align example files with current return-based API
    - example_minimal.php: Use direct return statements instead of Response->json()
    - validation.php: Updated Field rules with minLength, optional, required
    - Demonstrates current API patterns (return values, direct responses)

--- examples/example_minimal.php
+++ examples/example_minimal.php
@@ -3,19 +3,15 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Eorbahapi\EorbahAPI;
-use Eorbahapi\Response;
-use Eorbahapi\ExceptionHandlers;
 
 $app = new EorbahAPI('Example Minimal');
-$exceptionHandlers = new ExceptionHandlers();
-$exceptionHandlers->overrideExceptionHandlers($app);
 
-$app->get('/', function (Response $res) {
-    $res->json(['message' => 'Bienvenue sur EorbahAPI']);
+$app->get('/', function () {
+    return ['message' => 'Bienvenue sur EorbahAPI'];
 });
 
-$app->get('/items/{item_id}', function (Response $res, $item_id) {
-    $res->json(['item_id' => $item_id]);
+$app->get('/items/{item_id}', function ($item_id) {
+    return ['item_id' => $item_id];
 });
 
 $app->run();
--- examples/validation.php
+++ examples/validation.php
@@ -3,28 +3,34 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Eorbahapi\EorbahAPI;
-use Eorbahapi\Response;
-use Eorbahapi\ExceptionHandlers;
-use Eorbahapi\Validation\BaseModel;
+use Eorbahapi\Validator\BaseModel;
+use Eorbahapi\Validator\Field;
 
 $app = new EorbahAPI('Example Validation');
-$exceptionHandlers = new ExceptionHandlers();
-$exceptionHandlers->overrideExceptionHandlers($app);
 
 class ItemRequest extends BaseModel
 {
     public string $name;
     public float $price = 0.0;
     public bool $is_offer = false;
+
+    public static function fields(): array
+    {
+        return [
+            'name' => Field::required()->minLength(2),
+            'price' => Field::required()->min(0),
+            'is_offer' => Field::optional(),
+        ];
+    }
 }
 
-$app->put('/items/{item_id}', function (Response $res, ItemRequest $item, $item_id) {
-    $res->json([
+$app->put('/items/{item_id}', function (ItemRequest $item, $item_id) {
+    return [
         'item_id' => $item_id,
         'name' => $item->name,
         'price' => $item->price,
         'is_offer' => $item->is_offer,
-    ]);
+    ];
 });
 
 $app->run();
